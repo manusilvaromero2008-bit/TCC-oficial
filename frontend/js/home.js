49bece50dae953190a9a2326b52c73a9d500eb18
Manoela Romero <manu.silva.romero.2008@gmail.com>
2026-07-30 09:05:50 -0300

    modifiquei a home e as pages

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -2,25 +2,39 @@ const pesquisa = document.getElementById("pesquisa");
 const regiao = document.getElementById("regiao");
 const cards = document.querySelectorAll(".card");
 
-function filtrarClinicas(){
-    const textoPesquisa = pesquisa.ariaValueMax.toLowerCase();
-    const regiaoSelecionada = regiao.value;
+const paginas = {
+    "Pet Vida Veterinária": "petvida.html",
+    "Clínica Animal Care": "animalcare.html",
+    "VetCare Taquaral": "vetcare.html",
+    "Pet Health Barão": "pethealth.html"
+};
+
+function filtrar() {
+    const texto = pesquisa.value.toLowerCase();
+    const filtroRegiao = regiao.value.toLowerCase();
 
     cards.forEach(card => {
-        const nomeClinica = card.querySelector("h2").textContent.toLowerCase();
+        const nome = card.querySelector("h2").textContent.toLowerCase();
+        const reg = card.dataset.regiao.toLowerCase();
 
-        const regiaoCard = card.dataset.regiao;
+        const pesquisaOk = nome.includes(texto);
+        const regiaoOk = filtroRegiao === "todas" || reg === filtroRegiao;
 
-        const correspondePesquisa = nomeClinica.includes(textoPesquisa);
-        const correspondeRegiao = regiaoSelecionada === "todas" || regiaoSelecionada === regiaoCard;
-
-        if (correspondePesquisa && correspondeRegiao){
-            card.computedStyleMap.display = "block";
-        }else{
-            card.computedStyleMap.display = "none"
-        }
+        card.style.display = pesquisaOk && regiaoOk ? "block" : "none";
     });
 }
 
-pesquisa.addEventListener("input", filtrarClinicas);
-regiao.addEventListener("change", filtrarClinicas);
+pesquisa.addEventListener("input", filtrar);
+regiao.addEventListener("change", filtrar);
+
+cards.forEach(card => {
+    card.style.cursor = "pointer";
+
+    card.addEventListener("click", () => {
+        const nome = card.querySelector("h2").textContent;
+
+        if (paginas[nome]) {
+            window.location.href = paginas[nome];
+        }
+    });
+});
